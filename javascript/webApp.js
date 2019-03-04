013bc0bffd6ce58aa9a110e2079e2db0e480a784
Polleichtner Moritz <if170090@WS8A01.htl-leonding.ac.at>
2019-03-04 08:50:20 +0100

    undone

--- javascript/webApp.js
+++ javascript/webApp.js
@@ -1,10 +1,38 @@
+var netflx = 0;
+var amazon = 0;
 
-function CheckBox(form)
+function CheckBox()
 {
-  var test = 0;
-  if (form.box1.checked)
+
+  console.log(netflix);
+
+  console.log(amazon);
+
+  if (document.getElementById("1").checked == true)
   {
-    test++;
+    netflix++;
   }
-  console.log(test);
+  else if (document.getElementById("2").checked == true)
+  {
+    amazon++;
+  }
+  else if (document.getElementById("3").checked == true)
+  {
+    netflix++;
+  }
+
+  console.log(netflix);
+
+  console.log(netflix);
+
+
+  if (netflix > amazon)
+  {
+    console.log(netflix);
+  }
+  else
+  {
+    console.log(amazon);
+  }
+
 }
