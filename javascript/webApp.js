eca36d8b8c4bc9dd5dac8cab32d8f0fdd2fedd06
Polleichtner Moritz <if170090@WS8A01.htl-leonding.ac.at>
2019-03-08 08:50:21 +0100

    undone

--- javascript/webApp.js
+++ javascript/webApp.js
@@ -1,38 +1,160 @@
-var netflx = 0;
-var amazon = 0;
+const CLIENT_ID = '4cNswoNqM2wVFHPg';
 
-function CheckBox()
+const drone = new ScaleDrone(CLIENT_ID, {
+  data: { // Will be sent out as clientData via events
+    name: getRandomName(),
+    color: getRandomColor(),
+  },
+});
+
+let members = [];
+
+drone.on('open', error => {
+  if (error) {
+    return console.error(error);
+  }
+  console.log('Successfully connected to Scaledrone');
+
+  const room = drone.subscribe('observable-room');
+  room.on('open', error => {
+    if (error) {
+      return console.error(error);
+    }
+    console.log('Successfully joined room');
+  });
+
+  room.on('members', m => {
+    members = m;
+    updateMembersDOM();
+  });
+
+  room.on('member_join', member => {
+    members.push(member);
+    updateMembersDOM();
+  });
+
+  room.on('member_leave', ({id}) => {
+    const index = members.findIndex(member => member.id === id);
+    members.splice(index, 1);
+    updateMembersDOM();
+  });
+
+  room.on('data', (text, member) => {
+    if (member) {
+      addMessageToListDOM(text, member);
+    } else {
+      // Message is from server
+    }
+  });
+});
+
+drone.on('close', event => {
+  console.log('Connection was closed', event);
+});
+
+drone.on('error', error => {
+  console.error(error);
+});
+
+function getRandomName() {
+  var name = document.getElementById('1').value;
+  return (
+    adjs[Math.floor(Math.random() * adjs.length)] +
+    "_" +
+    nouns[Math.floor(Math.random() * nouns.length)]
+  );
+}
+
+function getRandomColor() {
+  return '#' + Math.floor(Math.random() * 0xFFFFFF).toString(16);
+}
+
+//------------- DOM STUFF
+
+const DOM = {
+  membersCount: document.querySelector('.members-count'),
+  membersList: document.querySelector('.members-list'),
+  messages: document.querySelector('.messages'),
+  input: document.querySelector('.message-form__input'),
+  form: document.querySelector('.message-form'),
+};
+
+DOM.form.addEventListener('submit', sendMessage);
+
+function sendMessage() {
+  const value = DOM.input.value;
+  if (value === '') {
+    return;
+  }
+  DOM.input.value = '';
+  drone.publish({
+    room: 'observable-room',
+    message: value,
+  });
+}
+
+function createMemberElement(member) {
+  const { name, color } = member.clientData;
+  const el = document.createElement('div');
+  el.appendChild(document.createTextNode(name));
+  el.className = 'member';
+  el.style.color = color;
+  return el;
+}
+
+function updateMembersDOM() {
+  DOM.membersCount.innerText = `${members.length} users in room:`;
+  DOM.membersList.innerHTML = '';
+  members.forEach(member =>
+    DOM.membersList.appendChild(createMemberElement(member))
+  );
+}
+
+function createMessageElement(text, member) {
+  const el = document.createElement('div');
+  el.appendChild(createMemberElement(member));
+  el.appendChild(document.createTextNode(text));
+  el.className = 'message';
+  return el;
+}
+
+function addMessageToListDOM(text, member) {
+  const el = DOM.messages;
+  const wasTop = el.scrollTop === el.scrollHeight - el.clientHeight;
+  el.appendChild(createMessageElement(text, member));
+  if (wasTop) {
+    el.scrollTop = el.scrollHeight - el.clientHeight;
+  }
+}
+
+/*function CheckBox()
 {
+  let netflix = 0;
+  let amazon = 0;
 
-  console.log(netflix);
-
-  console.log(amazon);
-
-  if (document.getElementById("1").checked == true)
+  if (document.getElementById("1").checked === true)
   {
     netflix++;
   }
-  else if (document.getElementById("2").checked == true)
+  else if (document.getElementById("2").checked === true)
   {
     amazon++;
   }
-  else if (document.getElementById("3").checked == true)
+  else if (document.getElementById("3").checked === true)
   {
     netflix++;
   }
-
-  console.log(netflix);
-
-  console.log(netflix);
-
-
-  if (netflix > amazon)
-  {
-    console.log(netflix);
-  }
-  else
-  {
-    console.log(amazon);
-  }
-
+  alert("recommended: "+netflix);
 }
+
+<form method="get">
+  <form id="myform">
+    <input type="checkbox" id="1"/> Netflix
+    <br>
+    <input type="checkbox" id="2"/> Amazon
+    <br>
+    <input type="checkbox" id="3"/> Netflix
+    <br>
+    <button type="submit" onsubmit="CheckBox()">submit</button>
+</form>
+<script type="text/javascript" src="../javascript/webApp.js"></script>*/
