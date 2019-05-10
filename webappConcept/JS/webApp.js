31cf547ea0aa50f2f4a12b123248ec2b9282fb79
Polleichtner Moritz <if170090@WS8A01.htl-leonding.ac.at>
2019-05-10 08:52:20 +0200

    undone

--- webappConcept/JS/webApp.js
+++ webappConcept/JS/webApp.js
@@ -1,10 +1,7 @@
 function tabulateAnswers() {
 
-  var c1score = 0;
-  var c2score = 0;
-  var c3score = 0;
-  var c4score = 0;
-
+  var netflix = 0;
+  var apv = 0;
 
   var choices = document.getElementsByTagName('input');
 
@@ -13,39 +10,25 @@ function tabulateAnswers() {
     if (choices[i].checked) {
 
       if (choices[i].value == 'c1') {
-        c1score = c1score + 1;
+        netflix++;
       }
       if (choices[i].value == 'c2') {
-        c2score = c2score + 1;
+        apv++;
       }
-      if (choices[i].value == 'c3') {
-        c3score = c3score + 1;
-      }
-      if (choices[i].value == 'c4') {
-        c4score = c4score + 1;
-      }
-
     }
   }
 
 
-  var maxscore = Math.max(c1score,c2score,c3score,c4score);
+  var maxscore = Math.max(netflix,apv);
 
 
   var answerbox = document.getElementById('answer');
-  if (c1score == maxscore) {
+  if (netflix == maxscore) {
     answerbox.innerHTML = "1";
   }
-  if (c2score == maxscore) {
+  if (apv == maxscore) {
     answerbox.innerHTML = "2";
   }
-  if (c3score == maxscore) {
-    answerbox.innerHTML = "3";
-  }
-  if (c4score == maxscore) {
-    answerbox.innerHTML = "4";
-  }
-
 }
 
 
@@ -58,7 +41,7 @@ function ShowBlock2()
 {
   document.getElementById("block1").style.visibility = "hidden";
   document.getElementById("block2").style.visibility = "visible";
-  document.getElementById("button1").style.display = "none";
+  document.getElementById("button1").style.visibility = "hidden";
   document.getElementById("button2").style.display = "flex";
 }
 
